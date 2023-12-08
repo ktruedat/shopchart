85bd4d2fd7a74b6827c01f9271d4ac073281da01
ktruedat <crinnic77@gmail.com>
2023-12-08 08:36:35 +0200

    finished producer api

--- api/controller/producer.py
+++ api/controller/producer.py
@@ -2,4 +2,42 @@ from abc import ABC, abstractmethod
 
 
 class IProducerRepository(ABC):
-    pass
+
+    @abstractmethod
+    def add_producer(self, producer):
+        pass
+
+    @abstractmethod
+    def get_producer(self, producer_id: int):
+        pass
+
+    def get_producers(self):
+        pass
+
+    @abstractmethod
+    def update_producer(self, producer_id: int, new_producer):
+        pass
+
+    @abstractmethod
+    def delete_producer(self, producer_id: int):
+        pass
+
+
+class ProducerController:
+    def __init__(self, producer_repository: IProducerRepository):
+        self.producer_repository = producer_repository
+
+    def add_producer(self, producer):
+        return self.producer_repository.add_producer(producer)
+
+    def get_producer(self, producer_id: int):
+        return self.producer_repository.get_producer(producer_id)
+
+    def get_producers(self):
+        return self.producer_repository.get_producers()
+
+    def update_producer(self, producer_id: int, new_producer):
+        return self.producer_repository.update_producer(producer_id, new_producer)
+
+    def delete_producer(self, producer_id: int):
+        return self.producer_repository.delete_producer(producer_id)
--- api/repository/producer.py
+++ api/repository/producer.py
@@ -1,34 +1,74 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
-
+from sqlalchemy.orm import sessionmaker
 from api.controller.producer import IProducerRepository
+from api.models import *
 from dbconnection import create_connection
 
 engine = create_connection()
 
+
 class ProducerRepository(IProducerRepository):
 
     def add_producer(self, producer):
-        new_producer = Producer(ProducerName=producer_name, ProducerLocation=producer_location)
+        Base.metadata.bind = engine
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        new_producer = Producer(
+            ProducerName=producer['Name'],
+            ProducerLocation=producer['Location']
+        )
         session.add(new_producer)
         session.commit()
+        session.close()
+
+        print(f"Product '{producer['Name']}' created successfully!")
+
         return new_producer
 
-    def read_producer(producer_id):
-        return session.query(Producer).get(producer_id)
+    def get_producer(self, producer_id: int):
+        Base.metadata.bind = engine
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    def update_producer(producer_id, new_name, new_location):
         producer = session.query(Producer).get(producer_id)
-        if producer:
-            producer.ProducerName = new_name
-            producer.ProducerLocation = new_location
-            session.commit()
+
+        session.close()
+
         return producer
 
-    def delete_producer(producer_id):
+    def get_producers(self):
+        Base.metadata.bind = engine
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        res_producers = session.query(Producer).all()
+        session.close()
+
+        return res_producers
+
+    def update_producer(self, producer_id: int, new_producer):
+        Base.metadata.bind = engine
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        producer = session.query(Producer).get(producer_id)
+        if producer:
+            producer.ProducerName = new_producer['Name']
+            producer.ProducerLocation = new_producer['Location']
+            session.commit()
+
+        session.close()
+        return producer
+
+    def delete_producer(self, producer_id: int):
+        Base.metadata.bind = engine
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
         producer = session.query(Producer).get(producer_id)
         if producer:
             session.delete(producer)
             session.commit()
-        return producer
 
+        session.close()
+        return producer
